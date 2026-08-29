75a30a6e21cbb04c5681cfba81a43088e8c9a123
Jen Code <314787904+Jen-Code2026@users.noreply.github.com>
2026-08-29 23:43:57 +0200

    empty lines in the Telegram message

--- weather_dresden.py
+++ weather_dresden.py
@@ -314,12 +314,11 @@ today_message = (
     f"Morgens: {morning} Grad, vormittags: "
     f"{late_morning} Grad, mittags: {noon} Grad, \nnachmittags: "
     f"{afternoon} Grad, abends: {evening} Grad\n\n"
-    f"Gewittergefahr: {thunderstorm_message}\n"
-    f"Niederschlag: {rain_message}\n"
-    f"Wind: {wind_speed_today} {today_winddirection}, "
+    f"Gewittergefahr: {thunderstorm_message}\n\n"
+    f"Niederschlag: {rain_message}\n\n"
+    f"Wind: {wind_speed_today} {today_winddirection}\n"
     f"Windböen bis zu {gusts_max_da_tomorrow} km/h, am stärksten" + 
      " und".join(gusts_periods_formatted2) + ".")
-print(today_message)
 
 bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
 channel_id = os.getenv("TELEGRAM_CHANNEL_ID")
